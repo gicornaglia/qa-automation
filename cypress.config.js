f4c4ef2d299095911045ea23e529f03fc33d8e15
Gisele Cornaglia <giselevzcornaglia@gmail.com>
2026-03-30 10:51:08 -0300

    ajustes

--- cypress.config.js
+++ cypress.config.js
@@ -3,11 +3,8 @@ require('dotenv').config()
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: process.env.CYPRESS_BASE_URL,
+    baseUrl: process.env.CYPRESS_BASE_URL || 'https://jsonplaceholder.typicode.com',
     specPattern: 'cypress/e2e/**/*.cy.js',
-    supportFile: 'cypress/support/e2e.js',
-    env: {
-      apiToken: process.env.CYPRESS_API_TOKEN
-    }
+    supportFile: 'cypress/support/e2e.js'
   }
 })
